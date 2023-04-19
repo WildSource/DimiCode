388ace8a306ea0945c3d736ea7e78c7c1d05338d
WildSource <ilovetrap123@hotmail.com>
2023-04-19 14:34:30 -0400

    basic javadoc lexer

--- src/lexer/Lexer.java
+++ src/lexer/Lexer.java
@@ -1,4 +1,11 @@
 package lexer;
 
+/**
+ * @author Samuel Nguyen-Phok
+ * @version 0.0.1
+ *
+ * This class read a file, tokenize every word and puts a label on it from the tokens enum.
+ *
+ */
 public class Lexer {
 }
