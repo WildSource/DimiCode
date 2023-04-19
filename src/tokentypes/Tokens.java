c759b4882227f1e84bcdd8f7fa56fd051a39b7a6
WildSource <ilovetrap123@hotmail.com>
2023-04-19 13:48:33 -0400

    add token types

---
+++ src/tokentypes/Tokens.java
@@ -0,0 +1,12 @@
+package tokentypes;
+
+/**
+ * @author Samuel Nguyen-Phok (WildSource)
+ * @version 0.0.1
+ *
+ * These are every possible type a token can possibly be
+ *
+ */
+public enum Tokens {
+    OPERATOR, DATA, VARIABLE, KEYWORD, PUNCTUATION;
+}
