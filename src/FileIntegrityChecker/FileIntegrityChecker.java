caa1490aa003e69f9aa2451fe636a0c22e7190d4
WildSource <ilovetrap123@hotmail.com>
2023-04-20 16:19:20 -0400

    refactor verifyFileExist and verifyFileExtension to FileIntegrityChecker

--- src/FileIntegrityChecker/FileIntegrityChecker.java
+++ src/FileIntegrityChecker/FileIntegrityChecker.java
@@ -1,5 +1,7 @@
 package FileIntegrityChecker;
 
+import java.io.File;
+
 /**
  * @author Samuel Nguyen-Phok
  * @version 0.0.1
@@ -8,4 +10,35 @@ package FileIntegrityChecker;
  * It is also the first step of the interpreter and acts as a bouncer to avoid working on a file that does not exist or the wrong type of file.
  */
 public class FileIntegrityChecker {
+    private File file;
+
+    public FileIntegrityChecker(String fileName) throws Exception {
+        this.file = new File(fileName);
+        verifyFileExist(this.file);
+        verifyFileExtension(this.file);
+    }
+
+    private void verifyFileExist(File file) throws Exception {
+        if (!file.exists()) {
+            throw new Exception("File does not exist");
+        } else {
+            System.out.println("File reference created");
+        }
+    }
+
+    private void verifyFileExtension(File file) throws Exception {
+        if (file.getName().endsWith(".dc")) {
+            System.out.println("File type verified");
+        } else {
+            throw new Exception("Wrong file type since it does not end with \".dc\" ");
+        }
+    }
+
+    public File getFile() {
+        return file;
+    }
+
+    public void setFile(File file) {
+        this.file = file;
+    }
 }
--- src/lexer/Lexer.java
+++ src/lexer/Lexer.java
@@ -6,31 +6,12 @@ import java.io.File;
  * @author Samuel Nguyen-Phok
  * @version 0.0.1
  * <p>
- * This class read a file, tokenize every word and puts a label on it from the tokens enum.
+ * This class read a file provided by the FileIntegrityChecker who has verified that the file works, tokenize every word and puts a label on them from the tokens enum.
  */
 public class Lexer {
     private File file;
 
     public Lexer(String fileName) throws Exception {
         this.file = new File(fileName);
-        verifyFileExist(this.file);
-        verifyFileExtensions(this.file);
-    }
-
-    private void verifyFileExist(File file) throws Exception {
-        if (!file.exists()) {
-            throw new Exception("File does not exist");
-        } else {
-            System.out.println("File reference created");
-        }
-    }
-
-    private void verifyFileExtensions(File file) throws Exception {
-        if (file.getName().endsWith(".dc")) {
-            System.out.println("File type verified");
-        } else {
-            throw new Exception("Wrong file type since it does not end with \".dc\" ");
-        }
-
     }
 }
