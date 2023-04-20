d0042a2f405a37fe4c55a6ad9146eae484ee7018
WildSource <ilovetrap123@hotmail.com>
2023-04-20 15:52:08 -0400

    add verifiyFileExist method

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import lexer.Lexer;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         new Lexer(args[0]);
     }
 }
--- src/lexer/Lexer.java
+++ src/lexer/Lexer.java
@@ -5,14 +5,22 @@ import java.io.File;
 /**
  * @author Samuel Nguyen-Phok
  * @version 0.0.1
- *
+ * <p>
  * This class read a file, tokenize every word and puts a label on it from the tokens enum.
- *
  */
 public class Lexer {
     private File file;
 
-    public Lexer(String fileName) {
+    public Lexer(String fileName) throws Exception {
         this.file = new File(fileName);
+        verifyFileExist(this.file);
+    }
+
+    private void verifyFileExist(File file) throws Exception {
+        if (!file.exists()) {
+            throw new Exception("File does not exist");
+        } else {
+            System.out.println("File reference created");
+        }
     }
 }
